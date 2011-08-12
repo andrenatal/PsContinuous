fc6c1206e1b97d6974d19b15eb05f91405a53aec
Andre Natal <andre@allapps.com.br>
2011-08-12 05:21:16 -0300

    Add DataBaseHelper

---
+++ psApiContinuous/src/com/pocketsphinxapi/consumer/DataBaseHelper.java
@@ -0,0 +1,123 @@
+package com.pocketsphinxapi.consumer;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DataBaseHelper extends SQLiteOpenHelper{
+ 
+    //The Android's default system path of your application database.
+    private static String DB_PATH = "/data/data/com.pocketsphinxapi.consumer/databases/";
+ 
+    private static String DB_NAME = "myDBName";
+ 
+    private SQLiteDatabase myDataBase; 
+ 
+    private final Context myContext;
+
+	private Resources resources;
+ 
+    /**
+     * Constructor
+     * Takes and keeps a reference of the passed context in order to access to the application assets and resources.
+     * @param context
+     */
+    public DataBaseHelper(Context context , String dbname , Resources _res) {
+ 
+    	
+    	super(context, dbname, null, 1);
+        this.myContext = context;
+        this.DB_NAME = dbname;
+        this.resources = _res;
+    }	
+ 
+
+ 
+    /**
+     * Check if the database already exist to avoid re-copying the file each time you open the application.
+     * @return true if it exists, false if it doesn't
+     */
+    private boolean checkDataBase(){
+ 
+    	SQLiteDatabase checkDB = null;
+ 
+    	try{
+    		String myPath = DB_PATH + DB_NAME;
+    		checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+ 
+    	}catch(SQLiteException e){
+ 
+    		//database does't exist yet.
+ 
+    	}
+ 
+    	if(checkDB != null){
+ 
+    		checkDB.close();
+ 
+    	}
+ 
+    	return checkDB != null ? true : false;
+    }
+ 
+ 
+    public void openDataBase() throws SQLException{
+ 
+    	//Open the database
+        String myPath = DB_PATH + DB_NAME;
+    	myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+ 
+    }
+ 
+    @Override
+	public synchronized void close() {
+ 
+    	    if(myDataBase != null)
+    		    myDataBase.close();
+ 
+    	    super.close();
+ 
+	}
+    
+    public List<String> selectAll(String word) {
+        List<String> list = new ArrayList<String>();
+        Cursor cursor = this.myDataBase.query(DB_NAME.substring(0 ,  DB_NAME.indexOf(".")  ), new String[] { "_id" , "fonema" },
+          "_id = '"+word+"' or _id like '"+word+"(%' ", null, null, null, null);
+        if (cursor.moveToFirst()) {
+           do {
+              list.add(cursor.getString(0) + "	" + cursor.getString(1));
+           } while (cursor.moveToNext());
+        }
+        if (cursor != null && !cursor.isClosed()) {
+           cursor.close();
+        }
+        return list;
+     }    
+ 
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+ 
+	}
+ 
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+ 
+	}
+ 
+        // Add your public helper methods to access and get content from the database.
+       // You could return cursors by doing "return myDataBase.query(....)" so it'd be easy
+       // to you to create adapters for your views.
+ 
+}
